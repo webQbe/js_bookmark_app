59900ce4b112ec8dbe1f537c0708ec256564c5ac
webQbe <webcubesolutionslk@gmail.com>
2024-10-09 20:12:54 +0530

    Added an alert for form validation

--- js/main.js
+++ js/main.js
@@ -8,6 +8,14 @@ function saveBookmark(e){
     var siteName = document.getElementById('siteName').value;
     var siteUrl = document.getElementById('siteUrl').value;
 
+    // form validation
+    // if siteName or siteUrl is null
+    if(!siteName || !siteUrl){
+
+        alert('Please fill in the form');
+        return false; // stop
+    }
+ 
     // create bookmark array object
     var bookmark = {
 
